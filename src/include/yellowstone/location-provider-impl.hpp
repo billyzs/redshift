f74caf10125068e7616d43c28480fdbc0593f25f
Billy Zhou <billyzs.728@gmail.com>
2017-12-10 21:58:39 -0500

    Add implementation of LocationProviderManual
    - set up base class and impl in C++
    - the new class isn't used in this commit
    - project still compiled, existing features unaffected
    - but later commits will try to use the new impl; if things break,
    revert to this commit

--- src/include/yellowstone/location-provider-impl.hpp
+++ src/include/yellowstone/location-provider-impl.hpp
@@ -19,35 +19,85 @@
 */
 #pragma once
 
-constexpr float     MIN_LAT         {-90.0};
-constexpr float     MAX_LAT         {90.0};
-constexpr float     MIN_LON         {-180.0};
-constexpr float     MAX_LON         {180.0};
+constexpr double     MIN_LAT         {-90.0};
+constexpr double     MAX_LAT         {90.0};
+constexpr double     MIN_LON         {-180.0};
+constexpr double     MAX_LON         {180.0};
 
-#include <iostream>
-#include <iomanip>
-template<typename T>
-struct Location {
+#include <string>
+#include <memory>
+
+// pedantically making these classes more complicated than they need to be
+template <typename T>
+class Location {
+private:
 	T lat;
 	T lon;
-	bool isValid(T){
-		if (!(lat >= MIN_LAT) || !(lat <= MAX_LAT))
-		{
-			std::cerr << std::setprecision(3) << "Latitude must be between" << MIN_LAT <<  "and " << MAX_LAT << std::endl;
-			return false;
-		}
-		else if (!(lon >= MIN_LON) || !(lon <= MAX_LON))
-		{
-			std::cerr << std::setprecision(3) << "Longitude must be between" << MIN_LON <<  "and " << MAX_LON << std::endl;
-			return false;
-		}
-		else
-			return true;
-	}
+	bool valid;
+public:
+	Location<T>(void) noexcept ;
+	Location<T>(const Location<T>& other) = default;
+	Location<T>(Location<T>&& other) = default;
+	constexpr Location<T>& operator=(const Location<T>& other) = default;
+	constexpr Location<T>& operator=(Location<T>&& other) = default;
+	void setLat(const T& lat_);
+	void setLon(const T& lon_);
+	bool isLatValid(const T &min, const T &max, const T &input) const;
+	bool isLonValid(const T &min, const T &max, const T &input) const ;
+	bool isValid(const T& lat_min = MIN_LAT, const T& lat_max = MAX_LAT,
+	             const T& lon_min = MIN_LON, const T& lon_max = MAX_LON) const;
+	std::string&& toStr() const;
 };
 
+// TODO turn this on
+using location_T = Location<double>;
+
+// mostly to hold data for each location provider
+class LocationProviderOption {
+public:
+	LocationProviderOption() = default;
+	//TODO why can't these be const?
+	LocationProviderOption(const location_T& loc_);
+	location_T loc;
+	mutable bool available{false};
+};
+
+// might become an issue if a custom deleter needs to be supplied
+using uPtrLPO = std::unique_ptr<LocationProviderOption>;
 class LocationProvider {
 public:
-	virtual void init() ;
+	// Ctors are never inherited; common tasks that are shared with all base class implementations can be put here,
+	// and base class ctors can call this
+	LocationProvider();
+
+	// Disable copy
+	LocationProvider(const LocationProvider& other) = delete;
+	LocationProvider& operator=(const LocationProvider& other) = delete;
+
+	// move is OK
+	LocationProvider& operator=(LocationProvider&& other) = default;
+	LocationProvider(LocationProvider&& other) = default;
+
+	virtual void init() const = 0;
+	virtual void start() = 0;
+	// This only needs to access the name, so make it static for now
+	virtual void printHelp() const = 0;
+	virtual ~LocationProvider() = default;
+
+};
+
+class LocationProviderManual : public LocationProvider {
+public:
+	// could be a variardic template?
+	LocationProviderManual();
+	LocationProviderManual(const LocationProviderOption& option_);
+	void init() const override;
+	void start() override;
+	void printHelp() const override ;
+	~LocationProviderManual() = default;
+	void setLat(const double& lat) noexcept;
+	void setLon(const double& lon) noexcept;
 private:
+	std::string name{"Manual"};
+	uPtrLPO option;
 };
---
+++ src/location-provider-impl.cpp
@@ -0,0 +1,115 @@
+/**
+    location-provider-impl.cpp
+    This file is part of yellowstone
+    yellowstone is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    
+    yellowstone is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    
+    You should have received a copy of the GNU General Public License
+    along with yellowstone. If not, see <http://www.gnu.org/licenses/>.
+    
+    Copyright (c) 2017-2017    Billy Zhou <billyzs.728@gmail.com>
+    Copyright (c) 2013-2017    Jon Lund Steffensen <jonlst@gmail.com>
+*/
+
+
+#include <iostream>
+#include <iomanip>
+#include <memory>
+#include <cmath>
+
+// #include <limits>
+
+#include <yellowstone/location-provider-impl.hpp>
+
+template<typename T>
+Location<T>::Location() noexcept :
+		lat{std::numeric_limits<T>::quiet_NaN()},
+        lon{std::numeric_limits<T>::quiet_NaN()},
+        valid{false} {};
+
+template <typename T>
+bool Location<T>::isLatValid(const T &min, const T &max, const T &input) const {
+	if ((input < MIN_LAT) || (input > MAX_LAT)) {
+		std::cerr << std::setprecision(4) << "Latitude must be between" << min <<  "and " << max << std::endl;
+		return false;
+	}
+	return true;
+}
+
+template <typename T>
+bool Location<T>::isLonValid(const T &min, const T &max, const T &input) const {
+	if ((input < min) || (input > max)) {
+		std::cerr << std::setprecision(4) << "Latitude must be between" << min << "and " << max << std::endl;
+		return false;
+	}
+	return true;
+}
+
+template <typename T>
+bool Location<T>::isValid(const T& lat_min, const T& lat_max, const T& lon_min, const T& lon_max) const {
+	return isLatValid(lat_min, lat_max, lat) && isLonValid(lon_min, lon_max, lon);
+}
+template  <typename T>
+std::string&& Location<T>::toStr() const {
+	std::ostringstream os;
+	os << std::setprecision(4) << "Location: "
+	   << fabs(lat) << ((lat > 0) ? "N" : "S")
+	   << ","
+	   << fabs(lon) << ((lon > 0) ? "E" : "W")
+	   << std::endl;
+	return os.str();
+}
+
+template <typename T>
+void Location<T>::setLat(const T &lat_) {
+	if (isLatValid(MIN_LAT, MAX_LAT, lat_)) {
+		lat = lat_;
+	}
+}
+
+template <typename T>
+void Location<T>::setLon(const T &lon_) {
+	if (isLonValid(MIN_LON, MAX_LON, lon_)) {
+		lon = lon_;
+	}
+}
+
+LocationProviderOption::LocationProviderOption(const location_T& loc_) : loc(loc_) {}
+LocationProvider::LocationProvider() {}; //NOP for now;
+
+LocationProviderManual::LocationProviderManual() :
+	option{std::make_unique<LocationProviderOption>()}
+{}
+
+LocationProviderManual::LocationProviderManual(const LocationProviderOption& option_) :
+	option{std::make_unique<LocationProviderOption>(option_)}
+{}
+
+void LocationProviderManual::init() const {
+	option->loc.isValid();
+}
+
+void LocationProviderManual::start() {
+	option->available = true;
+}
+void LocationProviderManual::printHelp() const {
+	std::cout << "Location Provider " << name << ":"<< std::endl
+	          << "Specify location manually in the format -l LAT:LON." << std::endl
+	          << "LAT and LON are expected to be floating point numbers." << std::endl
+			  << "negative values representing west / south, respectively" << std::endl;
+}
+
+void LocationProviderManual::setLat(const double &lat) noexcept {
+	option->loc.setLat(lat);
+}
+
+void LocationProviderManual::setLon(const double &lon) noexcept {
+	option->loc.setLon(lon);
+}
